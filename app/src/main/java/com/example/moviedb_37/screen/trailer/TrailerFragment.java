7771eabfcbbd6d3e44b80b74aade0faa02da14e9
Phung Van Kien <phungvankien1997@gmail.com>
2019-02-26 23:44:27 +0700

    create trailer movie fragment

--- app/src/main/java/com/example/moviedb_37/screen/trailer/TrailerFragment.java
+++ app/src/main/java/com/example/moviedb_37/screen/trailer/TrailerFragment.java
@@ -3,18 +3,26 @@ package com.example.moviedb_37.screen.trailer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.moviedb_37.R;
+import com.example.moviedb_37.databinding.FragmentTrailerBinding;
+import com.example.moviedb_37.screen.BaseFragment;
 
-public class TrailerFragment extends Fragment {
+public class TrailerFragment extends BaseFragment {
+    private FragmentTrailerBinding mBinding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_trailer, container, false);
+        View view = inflater.inflate(R.layout.fragment_trailer, container, false);
+        mBinding = FragmentTrailerBinding.bind(view);
+        mBinding.setViewModel(mViewModel);
+        setupAdapters(mBinding.recyclerTrailer,
+                new TrailerAdapter(mViewModel.getMovie().getVideoResult().getVideos()));
+        return view;
     }
 }
